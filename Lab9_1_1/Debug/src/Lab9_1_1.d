bea9a20e14d15185a89d44bb3d699fc08cd0c508
Stephen Walentik <swalenti@kent.edu>
2011-11-08 11:53:57 -0500

    This program uses vectors, the user types in a sentence and the program removes the repeated characters.

---
+++ Lab9_1_1/Debug/src/Lab9_1_1.d
@@ -0,0 +1,1 @@
+src/Lab9_1_1.d: ../src/Lab9_1_1.cpp
---
+++ Lab9_1_1/src/Lab9_1_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string tempString;
+	bool test=true;
+	char array[81] , temp;
+	vector<char> list;
+	cout << "Please enter your sentence: ";
+	getline(cin, tempString);
+	for (int count=0;count < tempString.size();count++)
+	{
+		temp = tempString.at(count);
+		for(int i = 0; i < list.size(); i++)
+			if(temp==list.at(i))
+				test = false;
+		if(test)
+			list.push_back(temp);
+		test=true;
+	}
+	for(int i=0;i<list.size();i++)
+		cout << list.at(i);
+	return 0;
+}
